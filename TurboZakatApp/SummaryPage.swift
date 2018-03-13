e3617ab4c458521f8099466ab6480f883d02fc8c
Hamza Lakhani <muhammadali@utmuser164-197.wireless.utoronto.ca>
2018-03-12 22:14:24 -0400

    demo1 version

---
+++ TurboZakatApp/SummaryPage.swift
@@ -0,0 +1,41 @@
+//
+//  SummaryPage.swift
+//  TurboZakatApp
+//
+//  Created by Hamza Lakhani on 2018-03-12.
+//  Copyright © 2018 Hamza Lakhani. All rights reserved.
+//
+
+import UIKit
+
+class SummaryPage: UIViewController {
+
+    var goldZakat = Double()
+    
+    @IBOutlet weak var goldFinalAmount: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        goldZakat = Double(goldFinalAmount.text)!
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
